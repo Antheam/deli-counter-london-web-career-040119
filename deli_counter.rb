fa3fd5064b4afa47e767ce01d4f58e3413863451
Anthea Merton <anthea.merton@gmail.com>
2019-03-21 17:08:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,12 @@
 katz_deli = []
 
 def line 
-  
+if   
 end 
+
+function nowServing (line) {
+  if (line.length===0) {
+    return`There is nobody waiting to be served!`;
+  }else{
+    return `Currently serving ${line.shift()}.`;
+  } }
