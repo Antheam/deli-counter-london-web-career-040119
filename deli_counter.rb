328d1852312e619f6797ec51888d07bbeaadcfc7
Anthea Merton <anthea.merton@gmail.com>
2019-03-21 17:15:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,15 @@
 # Write your code here.
 katz_deli = []
 
-def line 
-if deli.empty?
-    puts "The line is empty."
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |person, i|
       current_line << " #{i}. #{person}"
-end 
-
-puts current_line
+    end
+    puts current_line
   end
 end
 
@@ -27,4 +26,6 @@ def now_serving(deli)
     deli.shift
   end
 end
+
+
 
